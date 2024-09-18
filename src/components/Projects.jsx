62751ee514857a0ca798186ea1e228a0ec37f90f
Sharan HS <136172023+sharan-hs@users.noreply.github.com>
2024-09-18 16:11:11 +0530

    refactor: Updated Description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,25 +39,6 @@ const Projects = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        {/* <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {projectsList?.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md object-contain min-h-[400px]  duration-200 hover:scale-105"
-              />
-              <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
-                </button>
-              </div>
-            </div>
-          ))}
-        </div> */}
         <div className="flex flex-wrap gap-10">
           {projectsList?.map(({ id, src, weblink, githublink }) => (
             <div
