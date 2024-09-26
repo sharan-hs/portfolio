79e7fcb6b1e96328d780b8ed27adb1b6994b6256
Sharan HS <136172023+sharan-hs@users.noreply.github.com>
2024-09-26 15:08:44 +0530

    Updated Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/sharan_resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
